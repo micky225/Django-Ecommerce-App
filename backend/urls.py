b437ebe473c2af357d6143d6f202af809144e971
Stanislaus <geraldkhody@gmail.com>
2023-11-03 14:06:22 +0000

    Done with the sign up page

--- backend/urls.py
+++ backend/urls.py
@@ -4,5 +4,6 @@ from .import views
 app_name = 'Ecommerce_Console'
 
 urlpatterns = [
-    path('', views.home, name="name")
+    path('', views.home, name="name"),
+    path('signUp/', views.signUp),
 ]
--- backend/views.py
+++ backend/views.py
@@ -4,3 +4,6 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def signUp(request):
+    return render(request, 'signUp.html')
